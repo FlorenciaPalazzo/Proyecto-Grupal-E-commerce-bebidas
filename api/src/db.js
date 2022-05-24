65a233f2361a24b7429153c1b87c0e27370f3a5c
Natti.2012 <natti35443839@gmail.com>
2022-05-24 11:15:55 -0300

    realiondb

--- api/src/db.js
+++ api/src/db.js
@@ -30,11 +30,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { bebida, cliente } = sequelize.models;
+const { Bebida, Cliente } = sequelize.models;
 
 // Aca vendrian las relaciones
-cliente.hasMany(bebida);
-bebida.belongsTo(cliente);
+Cliente.hasMany(Bebida);
+Bebida.belongsTo(Cliente);
 // Product.hasMany(Reviews);
 
 
