3992bd88882b3be6ce71f13f26390490a0d283e8
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-05-24 19:17:41 -0300

    back-models-3 nuevos y 2 modificados

---
+++ api/src/models/producto.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('producto', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,     
+   },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+     
+    },
+    imagen: {
+      type: DataTypes.STRING,
+    },
+    marca: {
+      type: DataTypes.STRING,
+    },
+    ml: {
+      type: DataTypes.FLOAT,
+    },
+    graduacion: {
+      type: DataTypes.FLOAT,
+    },
+    precio: {
+      type: DataTypes.FLOAT,
+    },
+    stock: {
+      type: DataTypes.FLOAT,
+    }
+
+
+  });
+};
