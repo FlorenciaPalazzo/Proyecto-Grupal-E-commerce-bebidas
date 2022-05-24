d1ed3696a9987a5cfde52c713d11655f8df585c1
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-05-24 16:37:57 -0300

    back-rutas: GET api, GET bd y busqueda nombre

--- api/src/models/bebida.js
+++ api/src/models/bebida.js
@@ -12,7 +12,7 @@ module.exports = (sequelize) => {
     nombre: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+     
     },
     imagen: {
       type: DataTypes.STRING,
