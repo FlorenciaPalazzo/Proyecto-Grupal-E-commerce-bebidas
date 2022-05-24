6a4dbb590d2e8c7ec858add0bddc5ff29d2e0386
pedro marchetto <you@example.com>
2022-05-24 18:00:55 -0300

    front

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN_URL}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin + process.env.REACT_APP_PUBLIC_URL}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
