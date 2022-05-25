ddf0ef39ca81a14ea1ffc814dda293b33951d924
ValentinoMartz <valentinomartinez55@gmail.com>
2022-05-25 15:34:49 -0300

    solo un commit...

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,60 +1,56 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const axios = require ('axios')
+const axios = require("axios");
 
-const {Bebida}= require ('../db')
+const { Bebida } = require("../db");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 //cambiominimo
 
-
 const router = Router();
-
 
 // router.use('./bebidas' , bebidas)
 
-const getDataBase = async()=>{
-    return await Bebida.findAll() 
-}
-router.get('/bebidasApi', async (req, res, next) => {
-    
-    try {
-        
-        
-     const bebidasInfo = await axios.get(`https://bebidas-efc61-default-rtdb.firebaseio.com/results.json`)
-       
-     const allBebidas = await bebidasInfo.data.map(e => { return e })
-     
-     const allBebidasDb = await allBebidas.map(e => {
-         Bebida.create(e)
+const getDataBase = async () => {
+  return await Bebida.findAll();
+};
+router.get("/bebidasApi", async (req, res, next) => {
+  try {
+    const bebidasInfo = await axios.get(
+      `https://bebidas-efc61-default-rtdb.firebaseio.com/results.json`
+    );
+
+    const allBebidas = await bebidasInfo.data.map((e) => {
+      return e;
+    });
+
+    const allBebidasDb = await allBebidas.map((e) => {
+      Bebida.create(e);
+    });
+
+    res.json(allBebidas);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/bebidas", async (req, res, next) => {
+  try {
+    const { nombre } = req.query;
+    const dataInfo = await getDataBase();
+    if (nombre) {
+      const dataName = await dataInfo.filter((e) =>
+        e.nombre.toLowerCase().includes(nombre.toLowerCase())
+      );
+      if (!dataName.length) {
+        return res.status(400).send("No se encontro ese producto");
       }
-      )
-   
-      res.json(allBebidas)
-    } catch (error) {
-       next(error)
-    } 
-  });
-  router.get('/bebidas', async (req, res, next) => {
-     try {
-         
-      const {nombre} = req.query
-    const dataInfo = await getDataBase()
-    if(nombre){
-        const dataName = await dataInfo.filter(e=> e.nombre.toLowerCase().includes(nombre.toLowerCase()))
-        if(!dataName.length){
-            return res.status(400).send('No se encontro ese producto')
-        }
-        res.json(dataName)
-    }else{
-        res.json(dataInfo)
+      res.json(dataName);
+    } else {
+      res.json(dataInfo);
     }
-
-      } catch (error) {
-         next(error)
-     }
-  })
-
-
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
