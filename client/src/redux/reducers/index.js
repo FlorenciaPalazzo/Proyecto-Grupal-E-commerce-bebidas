512a8ecd6159c8adf90e51edca5cbc417f1d9856
ElGubb <gustybaig@hotmail.com>
2022-05-25 11:55:36 -0300

    añadido un action y reducer para conseguir productos por id

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,23 +1,31 @@
-import { ADMIN_HANDLER, GET_PRODUCT_NAME } from "../actions/actionsTypes";
+import { ADMIN_HANDLER, GET_PRODUCT_ID, GET_PRODUCT_NAME } from "../actions/actionsTypes";
 
 const initialState = {
-  isAdmin: false,
-  brands: [],
-  products: [],
-  productsSort: [],
+    isAdmin: false,
+    brands: [],
+    products: [],
+    productsSort: [],
+    detail: [],
 };
 
 export default function rootReducer(state = initialState, { type, payload }) {
-  switch (type) {
-    case ADMIN_HANDLER: {
-      console.log(process.env.REACT_APP_ADMIN_EMAIL, payload);
-      if (process.env.REACT_APP_ADMIN_EMAIL === payload) {
-        return { ...state, isAdmin: true };
-      } else return { ...state, isAdmin: false };
+    switch (type) {
+        case ADMIN_HANDLER:
+            {
+                console.log(process.env.REACT_APP_ADMIN_EMAIL, payload);
+                if (process.env.REACT_APP_ADMIN_EMAIL === payload) {
+                    return {...state, isAdmin: true };
+                } else return {...state, isAdmin: false };
+            }
+        case GET_PRODUCT_NAME:
+            return {...state, products: payload };
+
+        case GET_PRODUCT_ID:
+            return {...state, detail: payload }
+
+        default:
+            return state;
+
     }
-    case GET_PRODUCT_NAME:
-      return { ...state, products: payload };
-    default:
-      return state;
-  }
+
 }
