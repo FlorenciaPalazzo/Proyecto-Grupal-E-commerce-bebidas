d18e6c8ce6aa4aebaf944d03bf14a45bf5d53d3b
andresg19 <andygermain17@gmail.com>
2022-05-25 11:32:12 -0300

    AdminPanel

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Contact from "./components/Contact";
+import AdminPanel from "./components/AdminPanel";
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' exact element={<Home/>}/>
-        <Route path='/contact' element={<Contact/>}/>        
+        <Route path='/contact' element={<Contact/>}/>     
+        <Route path='/admin' element={<AdminPanel/>}/>   
       </Routes>
     </div>
   );
