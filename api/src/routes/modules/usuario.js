fba46f39502c42f33d414fe588989a0871ea7aad
Jonatan Alcaraz <jonatan.alcaraz9@gmail.com>
2022-06-05 21:48:06 -0300

    edfwsdf

--- api/src/routes/modules/usuario.js
+++ api/src/routes/modules/usuario.js
@@ -40,7 +40,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  let = { id, nombre, email, nacimiento, direccion, telefono, isAdmin } =
+  let = { id, nombre,apellido, email, nacimiento, direccion, telefono, isAdmin, image } =
     req.body;
   console.log("ruta", { id, nombre, email, nacimiento, direccion, telefono });
   try {
@@ -48,11 +48,13 @@ router.post("/", async (req, res) => {
       where: {
         id: id,
         nombre: nombre,
+        apellido: apellido,
         email: email,
         nacimiento: nacimiento ? nacimiento : null,
         direccion: direccion ? direccion : null,
         telefono: telefono ? telefono : null,
-        // isAdmin: isAdmin,
+        isAdmin: isAdmin,
+        image: image
       },
     });
     console.log("bien");
@@ -62,27 +64,27 @@ router.post("/", async (req, res) => {
     return res.status(400);
   }
 });
-router.put("/", async (req, res) => {
-  let {id, nombre, email, contraseña, nacimiento, direccion, telefono, image } = req.body.user;
-  console.log(req.body.user);
-  try {
-    const usuarioPut = await Usuario.findOne({ where: { id: id } });
-    console.log("usuarioPut busqueda", usuarioPut);
-    let updated = await usuarioPut.update({
-      id: id || usuarioPut.id,
-      nombre: nombre || usuarioPut.nombre,
-      email: email || usuarioPut.email,
-      contraseña: contraseña || usuarioPut.contraseña,
-      nacimiento: nacimiento || usuarioPut.nacimiento,
-      direccion: direccion || usuarioPut.direccion,
-      telefono: telefono || usuarioPut.telefono,
-      image: image || usuarioPut.image,
-    });
-    res.json(updated);
-  } catch (err) {
-    console.log(err);
-  }
-});
+ router.put("/", async (req, res) => {
+    let {id, nombre, email, contraseña, nacimiento, direccion, telefono, image } = req.body.user;
+    console.log(req.body.user);
+    try {
+      const usuarioPut = await Usuario.findOne({ where: { id: id } });
+      console.log("usuarioPut busqueda", usuarioPut);
+      let updated = await usuarioPut.update({
+        id: id || usuarioPut.id,
+        nombre: nombre || usuarioPut.nombre,
+        email: email || usuarioPut.email,
+        contraseña: contraseña || usuarioPut.contraseña,
+        nacimiento: nacimiento || usuarioPut.nacimiento,
+        direccion: direccion || usuarioPut.direccion,
+        telefono: telefono || usuarioPut.telefono,
+        image: image || usuarioPut.image,
+      });
+      res.json(updated);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 //------Mercado Pago-----
 
