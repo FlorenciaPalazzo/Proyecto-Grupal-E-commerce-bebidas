854d07f0abbc823b5c5eca04f531933e59b2a0c3
ValentinoMartz <valentinomartinez55@gmail.com>
2022-06-07 22:03:19 -0300

    avances en favoritos, por ahora a media hasta

--- client/src/components/Favoritos/index.jsx
+++ client/src/components/Favoritos/index.jsx
@@ -2,23 +2,29 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { deleteFavorito, getFavorito } from "../../redux/actions";
+import Loading from "../Loading";
 
 export const Favoritos = () => {
   const elFavorito = useSelector((state) => state.favProducts);
-  //const usuario = useSelector((state) => state.currentUser);
+  const loading = useSelector((state) => state.isLoading);
+  //const product = useSelector((state) => state.products);
+  const [bool, setBool] = useState(true);
 
-  console.log("EL FAVORITO", elFavorito);
   const dispatch = useDispatch();
 
   let user = localStorage.getItem("user");
 
-  console.log("SOY EL USUARIO--->", user);
-
   useEffect(() => {
-    if (!elFavorito.length) {
+    console.log("Antes del if", bool);
+    console.log("Elfavorito antes del if", elFavorito);
+    if (bool && elFavorito) {
       dispatch(getFavorito(user));
+      setBool(false);
     }
-  }, [elFavorito]);
+    console.log("Elfavorito despues del if", elFavorito);
+
+    console.log("despues del if", bool);
+  }, [dispatch, elFavorito, bool]);
 
   const handleDeleteFav = (e) => {
     e.preventDefault();
@@ -26,29 +32,40 @@ export const Favoritos = () => {
     let payload = { id_prod: idProd, id_user: user };
 
     dispatch(deleteFavorito(payload));
+    setBool(true);
   };
   return (
     <div>
-      <Link to="/">
-        <button className="button">Home</button>
-      </Link>
-      <div>Lista de Favoritos</div>
-
-      {elFavorito.length > 0 ? (
-        elFavorito.map((e) => {
-          return (
-            <div key={e.id}>
-              <button className="button" value={e.id} onClick={handleDeleteFav}>
-                Borrar
-              </button>
-              {e.nombre}
-              <img src={e.imagen} />
-            </div>
-          );
-        })
+      {loading /* revisen esto!! */ ? (
+        <Loading />
       ) : (
         <div>
-          <h2>No hay favoritos</h2>
+          <Link to="/">
+            <button className="button">Home</button>
+          </Link>
+          <div>Lista de Favoritos</div>
+
+          {elFavorito.length > 0 ? (
+            elFavorito.map((e) => {
+              return (
+                <div key={e.id}>
+                  <button
+                    className="button"
+                    value={e.id}
+                    onClick={handleDeleteFav}
+                  >
+                    Borrar
+                  </button>
+                  {e.nombre}
+                  <img src={e.imagen} width="20%" />
+                </div>
+              );
+            })
+          ) : (
+            <div>
+              <h2>No hay favoritos</h2>
+            </div>
+          )}
         </div>
       )}
     </div>
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -42,7 +42,7 @@ import {
   DELETE_DIRECCIONES,
   ADD_HIST,
   GET_HIST,
-  CLEAR_STATE
+  CLEAR_STATE,
   //---------> prueba!!!
 } from "../actions/actionsTypes";
 
@@ -70,7 +70,8 @@ const initialState = {
   reviewPage: [],
   allReviews: [],
   favProducts: [],
-  historial : []
+  historial: [],
+  favBoolean: [],
 };
 
 export default function rootReducer(state = initialState, { type, payload }) {
@@ -102,7 +103,7 @@ export default function rootReducer(state = initialState, { type, payload }) {
         return { ...state, isAdmin: true };
       } else return { ...state, isAdmin: false };
     }
-   
+
     case GET_PRODUCT_NAME:
       return { ...state, products: payload, searchProduct: payload };
 
@@ -208,8 +209,8 @@ export default function rootReducer(state = initialState, { type, payload }) {
           products: state.productsSort.filter(
             (e) => e.ml >= 950 && e.ml < 1500
           ),
-        }
-      };
+        };
+      }
     case FILTER_BY_PRICE:
       if (payload === "all") {
         return {
@@ -406,24 +407,22 @@ export default function rootReducer(state = initialState, { type, payload }) {
         ...state,
       };
     case SET_FAV:
-      return { ...state, favProducts: payload };
+      return { ...state };
 
     case GET_FAV:
-      let productos = state.products; //te trae todos los productos
+      let productos = state.products;
+      let ids = payload.map((e) => e.productoId);
 
-      let ids = payload.map((e) => e.productoId); //mapea los prod fav
       let arr = [];
-      console.log("SOY EL PAYLOAD", payload);
 
-      productos.map((e) => {
+      productos.forEach((e) => {
         //mapea productos pregunta si hay id prod
+        console.log(e);
         if (ids.includes(e.id)) {
+          console.log("e", e);
           arr.push(e);
         }
       });
-
-      console.log("SOY EL FILTRO PROD", arr);
-
       return {
         ...state,
         favProducts: arr,
@@ -433,44 +432,42 @@ export default function rootReducer(state = initialState, { type, payload }) {
       return { ...state, favProducts: payload };
     case ADD_DIRECCIONES:
       return {
-        ...state
+        ...state,
       };
-      case GET_DIRECCIONES:
-        return{
-          ...state,
-          direcciones: payload.direcciones
-      }
+    case GET_DIRECCIONES:
+      return {
+        ...state,
+        direcciones: payload.direcciones,
+      };
 
-      case DELETE_DIRECCIONES:
-        return{
-          ...state,
-         
-      }
+    case DELETE_DIRECCIONES:
+      return {
+        ...state,
+      };
 
+    case GET_HIST:
+      // console.log(payload, "Soy payloaff")
+      let prodHist = state.products;
 
-    case GET_HIST : 
-    // console.log(payload, "Soy payloaff")
-    let prodHist = state.products;
+      // console.log(prodHist, "soy los productos")
 
-    // console.log(prodHist, "soy los productos")
-    
-    let idHist = payload.map((e) => e.productoId)
-    // console.log(idHist, "soy los Los IDS")
-    let histArr = []
+      let idHist = payload.map((e) => e.productoId);
+      // console.log(idHist, "soy los Los IDS")
+      let histArr = [];
 
-    prodHist.map((e) => {
-      if(idHist.includes(e.id)){
-        histArr.push(e)
-      }
-    })
-    // console.log(histArr, "Teoricamente esto debería andar bien")
-    return{
-      ...state ,
-      historial : histArr
-    }
+      prodHist.map((e) => {
+        if (idHist.includes(e.id)) {
+          histArr.push(e);
+        }
+      });
+      // console.log(histArr, "Teoricamente esto debería andar bien")
+      return {
+        ...state,
+        historial: histArr,
+      };
 
-    case ADD_HIST : 
-    return {...state}
+    case ADD_HIST:
+      return { ...state };
 
     case CLEAR_STATE:
       return {
