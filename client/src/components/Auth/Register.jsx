9b434e8a008b93b5d49ed5ac4562d3c38009a7ce
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-06-13 13:43:38 -0300

    registro terminado

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -13,6 +13,8 @@ import { Link } from "react-router-dom";
 import Loading from "../Loading";
 import "./Register.css";
 
+import NavBarSec from "../NavBarSec";
+
 function Register() {
   // { id, nombre, email, nacimiento, direccion, telefono }
   const [input, setInput] = useState({
@@ -109,140 +111,153 @@ function Register() {
   console.log(input);
 
   return (
-    <div className="registro-body">
-      {loading && !isLoged ? (
-        <Loading />
-      ) : (
-        <div className="registro-cont">
-          <div>
-            {error && <span>{error}</span>}
-            <form onSubmit={handleSubmit}>
-              <div className="registro-form">
-                <label htmlFor="nombre">Nombre</label>
-                <input
-                  type="text"
-                  name="nombre"
-                  placeholder="Enter your name"
-                  onChange={handleChange}
-                />
-              </div>
+    <div>
+      <NavBarSec />
+      <div className="registro-body">
+        <div className="registro-img"></div>
+        {loading && !isLoged ? (
+          <Loading />
+        ) : (
+          <div className="registro-cont">
+            <div className="registro-form">
+              <p className="registro-titulo">Registrate</p>
+            </div>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className="registro-form">
+                  <label htmlFor="nombre">Nombre</label>
+                  <input
+                    type="text"
+                    name="nombre"
+                    placeholder="Ingrese su nombre"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              {nameError && <span>{nameError}</span>}
+                <div className="registro-form">
+                  <label htmlFor="apellido">Apellido</label>
+                  <input
+                    type="text"
+                    name="apellido"
+                    placeholder="Ingrese su apellido"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              <div className="registro-form">
-                <label htmlFor="apellido">Apellido</label>
-                <input
-                  type="text"
-                  name="apellido"
-                  placeholder="Enter your surname"
-                  onChange={handleChange}
-                />
-              </div>
+                <div className="registro-form">
+                  <label htmlFor="telefono">Celular</label>
+                  <input
+                    type="tel"
+                    name="telefono"
+                    placeholder="Ingrese su teléfono"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              {surnameError && (
-                <span className="register-span">{surnameError}</span>
-              )}
+                <div className="registro-form">
+                  <label htmlFor="nacimiento">Nacimiento</label>
+                  <input
+                    type="text"
+                    name="nacimiento"
+                    placeholder="dd/mm/yyyy"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              <div className="registro-form">
-                <label htmlFor="telefono">Celular</label>
-                <input
-                  type="tel"
-                  name="telefono"
-                  placeholder="Enter your phone number"
-                  onChange={handleChange}
-                />
-              </div>
+                <div className="registro-form">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Ingrese su email"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              <div className="registro-form">
-                <label htmlFor="nacimiento">Nacimiento</label>
-                <input
-                  type="text"
-                  name="nacimiento"
-                  placeholder="dd/mm/yyyy"
-                  onChange={handleChange}
-                />
-              </div>
+                <div className="registro-form">
+                  <label htmlFor="password">Contraseña</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Ingrese su contraseña"
+                    onChange={handleChange}
+                  />
+                </div>
 
-              {birthError && <span>{birthError}</span>}
+                <div className="registro-form">
+                  <label htmlFor="confirmPassword">
+                    Confirma tu contraseña
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    onChange={handleChange}
+                    placeholder="Confirme su contraseña"
+                  />
+                </div>
 
-              <div className="registro-form">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  onChange={handleChange}
-                />
-              </div>
+                {!passwordError &&
+                !surnameError &&
+                !nameError &&
+                !emailError &&
+                !birthError &&
+                input.nombre &&
+                input.apellido &&
+                input.password &&
+                input.nacimiento &&
+                input.email ? (
+                  <button className="registro-btn">Registrarse</button>
+                ) : (
+                  <button
+                    className="registro-btn"
+                    type="button"
+                    onClick={() => alert("Complete todos los campos")}
+                  >
+                    Registrarse
+                  </button>
+                )}
 
-              {emailError && <span>{emailError}</span>}
-
-              <div className="registro-form">
-                <label htmlFor="password">Contraseña</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={handleChange}
-                />
-              </div>
-
-              {passwordError && <span>{passwordError}</span>}
-
-              <div className="registro-form">
-                <label htmlFor="confirmPassword">Confirma tu contraseña</label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                  onChange={handleChange}
-                />
-              </div>
-
-              {confirmPasswordError && <span>{confirmPasswordError}</span>}
-
-              {!passwordError &&
-              !surnameError &&
-              !nameError &&
-              !emailError &&
-              !birthError &&
-              input.nombre &&
-              input.apellido &&
-              input.password &&
-              input.nacimiento &&
-              input.email ? (
-                <button>Registrarse</button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={() => alert("Complete todos los campos")}
-                >
-                  Registrarse
-                </button>
-              )}
-
-              <div class="text-center">
-                <p>or sign up with:</p>
-                <button type="button" class="btn btn-primary btn-floating mx-1">
-                  <i class="fab fa-facebook-f"></i>
-                </button>
-
-                <button type="button" class="btn btn-primary btn-floating mx-1">
-                  <i class="fab fa-google"></i>
-                </button>
-
-                <button type="button" class="btn btn-primary btn-floating mx-1">
-                  <i class="fab fa-twitter"></i>
-                </button>
-
-                <button type="button" class="btn btn-primary btn-floating mx-1">
-                  <i class="fab fa-github"></i>
-                </button>
-              </div>
-            </form>
+                <div>
+                  <p className="registro-text">o registrate con Google</p>
+                  <button className="registro-btn-google">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      fill="white"
+                      class="bi bi-google"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                    </svg>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
+        )}
+
+        <div className="registro-error">
+          {error && <span>{error}</span>}
+
+          {nameError && <span>{nameError}</span>}
+          <br />
+          {surnameError && (
+            <span className="register-span">{surnameError}</span>
+          )}
+          <br />
+          {birthError && <span>{birthError}</span>}
+          <br />
+
+          {emailError && <span>{emailError}</span>}
+          <br />
+          {passwordError && <span>{passwordError}</span>}
+          <br />
+          {confirmPasswordError && <span>{confirmPasswordError}</span>}
         </div>
-      )}
+      </div>
     </div>
   );
 }
