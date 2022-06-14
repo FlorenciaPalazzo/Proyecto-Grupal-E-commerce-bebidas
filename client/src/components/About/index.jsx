4e3d5f6c112b36d788f93064b2e42a8642061afc
ValentinoMartz <valentinomartinez55@gmail.com>
2022-06-14 02:00:23 -0300

    about faltan las fotos

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,45 +1,27 @@
 import React from "react";
+import NavBarSec from "../NavBarSec";
+/* import Footer from "../Footer"; */
 
 export const About = () => {
   return (
     <div>
-      {/*  <header class="masthead text-center text-white">
-        <div class="masthead-content">
-          <div class="container px-5">
-            <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-            <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">
-              Learn More
-            </a>
-          </div>
-        </div>
-        <div class="bg-circle-1 bg-circle"></div>
-        <div class="bg-circle-2 bg-circle"></div>
-        <div class="bg-circle-3 bg-circle"></div>
-        <div class="bg-circle-4 bg-circle"></div>
-      </header> */}
+      <NavBarSec />
 
       <section id="scroll">
         <div class="container px-5">
           <div class="row gx-5 align-items-center">
             <div class="col-lg-6 order-lg-2">
               <div class="p-5">
-                <img
-                  class="img-fluid rounded-circle"
-                  src="assets/img/01.jpg"
-                  alt="..."
-                />
+                <img class="img-fluid rounded-circle" src="/images/ja.jpeg" />
               </div>
             </div>
             <div class="col-lg-6 order-lg-1">
               <div class="p-5">
-                <h2 class="display-4">For those about to rock...</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
-                  aliquid, mollitia odio veniam sit iste esse assumenda amet
-                  aperiam exercitationem, ea animi blanditiis recusandae!
-                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
-                </p>
+                <h2 class="display-4">Jonatan Alcaraz</h2>
+                <a href="https://github.com/ValentinoMartz">GitHub</a> <br />
+                <a href="https://www.linkedin.com/in/valentinomartz/">
+                  Linkedin
+                </a>
               </div>
             </div>
           </div>
@@ -52,7 +34,7 @@ export const About = () => {
               <div class="p-5">
                 <img
                   class="img-fluid rounded-circle"
-                  src="assets/img/02.jpg"
+                  src="/images/pm.jpeg"
                   alt="..."
                 />
               </div>
@@ -78,7 +60,7 @@ export const About = () => {
               <div class="p-5">
                 <img
                   class="img-fluid rounded-circle"
-                  src="assets/img/03.jpg"
+                  src="/images/ag.jpeg"
                   alt="..."
                 />
               </div>
@@ -96,18 +78,135 @@ export const About = () => {
             </div>
           </div>
         </div>
-      </section>
-      <footer class="py-5 bg-black">
         <div class="container px-5">
-          <p class="m-0 text-center text-white small">
-            Copyright &copy; Your Website 2022
-          </p>
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="/images/ja.jpeg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2 class="display-4">We salute you!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </footer>
+      </section>
+      <section>
+        <div class="container px-5">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="/images/ja.jpeg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2 class="display-4">Let there be rock!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container px-5">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="/images/ja.jpeg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2 class="display-4">We salute you!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="container px-5">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="/images/ja.jpeg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2 class="display-4">Let there be rock!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container px-5">
+          <div class="row gx-5 align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5">
+                <img
+                  class="img-fluid rounded-circle"
+                  src="/images/ja.jpeg"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2 class="display-4">We salute you!</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                  aliquid, mollitia odio veniam sit iste esse assumenda amet
+                  aperiam exercitationem, ea animi blanditiis recusandae!
+                  Ratione voluptatum molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+      {/*<Footer /> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-      <script src="js/scripts.js"></script>
+      <script src="js/scripts.js"></script> */}
     </div>
   );
 };
