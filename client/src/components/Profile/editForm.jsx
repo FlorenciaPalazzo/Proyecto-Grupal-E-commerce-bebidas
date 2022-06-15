fa7f4ba714638d601f866c5f8e0a0bd57f05a2b8
ValentinoMartz <valentinomartinez55@gmail.com>
2022-06-15 16:52:34 -0300

    userprofile update

--- client/src/components/Profile/editForm.jsx
+++ client/src/components/Profile/editForm.jsx
@@ -15,7 +15,7 @@ export default function EditForm() {
   const user = useSelector((state) => state.currentUser);
   const dbUser = useSelector((state) => state.dbUser);
 
-const loading = useSelector((state) => state.isLoading);
+  const loading = useSelector((state) => state.isLoading);
   const [disabledBtn, setDisabledBtn] = useState(false);
   const [nameError, setNameError] = useState(null);
   const [surnameError, setSurnameError] = useState(null);
@@ -23,7 +23,7 @@ const loading = useSelector((state) => state.isLoading);
     nombre: "",
     apellido: "",
   });
- 
+
   const [image, setImage] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -73,14 +73,12 @@ const loading = useSelector((state) => state.isLoading);
         });
         console.log(error);
       });
-     
+
     navigate("/profile");
   }
   // console.log("user selector", user);
   // console.log("imagen", dbUser);
 
-
-  
   useEffect(() => {
     if (user) {
       dispatch(getUserDb(user.uid));
@@ -102,125 +100,136 @@ const loading = useSelector((state) => state.isLoading);
     }
     return () => {
       if (user && user.isAdmin) {
-      window.location.reload("/profile") 
-         navigate("/profile");
+        window.location.reload("/profile");
+        navigate("/profile");
       }
-      console.log("Aactualizo y me voy a la mierda");
+      console.log("Actualizo y me voy a la mierda");
       dispatch(resetUserDb());
     };
   }, [user]);
   console.log(input, user);
   return (
     <div>
-      <NavBarSec/>
+      <NavBarSec />
       {loading /* revisen esto!! */ ? (
         <Loading />
       ) : (
-    <div class="container">
-      <h1>Editar Perfil</h1>
-      <hr />
-      <div class="row">
-        <div class="col-md-3">
-          <div class="text-center">
-            <h6>Elige una foto diferente</h6>
-            <div className="base64back">
-              <FileBase64
-                type="file"
-                multiple={false} //
-                onDone={({ base64 }) => setImage(base64)}
-              />
-            </div>
-            {dbUser&& (
-              <img src={dbUser.image} class="avatar img-circle" alt="avatar" />
-            )}
-          </div>
-        </div>
-        {/* Hasta aca andamos */}
-        <div class="col-md-9 personal-info">
-          <h3>Informacion personal</h3>
-          <form class="form-horizontal" role="form">
-            <div class="form-group">
-              <label class="col-lg-3 control-label" htmlFor="nombre">
-                Nombre:{" "}
-              </label>
-              <div class="col-lg-8">
-                <input
-                  type="text"
-                  value={input.nombre}
-                  name="nombre"
-                  onChange={handleChange}
-                  class="form-control"
-                />
-              </div>
-              {nameError && <span>{nameError}</span>}
-            </div>
-
-            <div class="form-group">
-              <label class="col-lg-3 control-label" htmlFor="apellido">
-                Apellido:{" "}
-              </label>
-              <div class="col-lg-8">
-                <input
-                  type="text"
-                  value={input.apellido}
-                  name="apellido"
-                  onChange={handleChange}
-                  class="form-control"
-                />
-                {surnameError && <span>{surnameError}</span>}
-                <br />
-                <div class="form-group" id="passwordChange">
-                  <br />
-
-                  <label class="col-lm-3  control-label " htmlFor="password">
-                    Cambiar contraseña mediante email:{" "}
-                  </label>
-                  {!disabledBtn ? (
-                    <button class="btn btn-dark" onClick={passwordHandle}>
-                      Solicitar Cambio
-                    </button>
-                  ) : (
-                    <div>
-                      <button
-                        class="btn btn-dark"
-                        disabled
-                        onClick={passwordHandle}
-                      >
-                        Solicitar Cambio
-                      </button>
-                      <span> Se envio email para restablecer contraseña</span>
-                    </div>
-                  )}
+        <div class="container">
+          <h1>Editar Perfil</h1>
+          <hr />
+          <div class="row">
+            <div class="col-md-3">
+              <div class="text-center">
+                <h6>Elige una foto diferente</h6>
+                <div className="base64back">
+                  <FileBase64
+                    type="file"
+                    multiple={false} //
+                    onDone={({ base64 }) => setImage(base64)}
+                  />
                 </div>
-                <br />
-                {surnameError || nameError ? (
-                  <button
-                    type="button"
-                    onClick={() =>
-                      alert("Complete todos los campos correctamente")
-                    }
-                  >
-                    Guardar
-                  </button>
-                ) : (
-                  <button
-                    class="btn btn-primary"
-                    type="submit"
-                    onClick={handleSubmit}
-                  >
-                    Guardar
-                  </button>
+                {dbUser && (
+                  <img
+                    src={dbUser.image}
+                    class="avatar img-circle"
+                    alt="avatar"
+                  />
                 )}
               </div>
             </div>
+            {/* Hasta aca andamos */}
+            <div class="col-md-9 personal-info">
+              <h3>Informacion personal</h3>
+              <form class="form-horizontal" role="form">
+                <div class="form-group">
+                  <label class="col-lg-3 control-label" htmlFor="nombre">
+                    Nombre:{" "}
+                  </label>
+                  <div class="col-lg-8">
+                    <input
+                      type="text"
+                      value={input.nombre}
+                      name="nombre"
+                      onChange={handleChange}
+                      class="form-control"
+                    />
+                  </div>
+                  {nameError && <span>{nameError}</span>}
+                </div>
 
-            <br />
-            <br />
-          </form>
-          {user && <img src={user.photoURL} style={{ width: "20%" }} />}
+                <div class="form-group">
+                  <label class="col-lg-3 control-label" htmlFor="apellido">
+                    Apellido:{" "}
+                  </label>
+                  <div class="col-lg-8">
+                    <input
+                      type="text"
+                      value={input.apellido}
+                      name="apellido"
+                      onChange={handleChange}
+                      class="form-control"
+                    />
+                    {surnameError && <span>{surnameError}</span>}
+                    <br />
+                    <div class="form-group" id="passwordChange">
+                      <br />
+
+                      <label
+                        class="col-lm-3  control-label "
+                        htmlFor="password"
+                      >
+                        Cambiar contraseña mediante email:{" "}
+                      </label>
+                      {!disabledBtn ? (
+                        <button class="btn btn-dark" onClick={passwordHandle}>
+                          Solicitar Cambio
+                        </button>
+                      ) : (
+                        <div>
+                          <button
+                            class="btn btn-dark"
+                            disabled
+                            onClick={passwordHandle}
+                          >
+                            Solicitar Cambio
+                          </button>
+                          <span>
+                            {" "}
+                            Se envio email para restablecer contraseña
+                          </span>
+                        </div>
+                      )}
+                    </div>
+                    <br />
+                    {surnameError || nameError ? (
+                      <button
+                        type="button"
+                        onClick={() =>
+                          alert("Complete todos los campos correctamente")
+                        }
+                      >
+                        Guardar
+                      </button>
+                    ) : (
+                      <button
+                        class="btn btn-primary"
+                        type="submit"
+                        onClick={handleSubmit}
+                      >
+                        Guardar
+                      </button>
+                    )}
+                  </div>
+                </div>
+
+                <br />
+                <br />
+              </form>
+              {user && <img src={user.photoURL} style={{ width: "20%" }} />}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>)}
+      )}
     </div>
   );
 }
