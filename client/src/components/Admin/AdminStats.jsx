7b7a7c75e14785135447355bb7455f0bf6c78f93
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-06-16 00:54:45 -0300

    .............

--- client/src/components/Admin/AdminStats.jsx
+++ client/src/components/Admin/AdminStats.jsx
@@ -12,23 +12,7 @@ export const AdminStats = () => {
   return (
     <div>
       <AdminPanel />
-      <div class="body-stats">
-        <div class="containter-stats">
-          <div class="card-stats">Ventas: {stat.ventas}</div>
-          <div class="card-stats">Productos: {stat.productos}</div>
-          <div class="card-stats">Reviews totales: {stat.totalReview}</div>
-          <div class="card-stats">Usuarios: {stat.usuarios}</div>
-          <div class="card-stats">
-            Usuarios verificados: {stat.verifiedUser}
-          </div>
-          <div class="card-stats">
-            Usuarios no verificados: {stat.noVerifiedUser}
-          </div>
-          <div class="card-stats">Promedio de la pagina: {stat.pageProm}</div>
-          <div class="card-stats">Reviews de productos: {stat.userReviews}</div>
-          <div class="card-stats">Review de la página: {stat.pageReviews}</div>
-        </div>
-      </div>
+      <div className="stats-cont"></div>
     </div>
   );
 };
