ac70cf75841f121bfa2fb748f40c135de518ef1d
ValentinoMartz <valentinomartinez55@gmail.com>
2022-06-16 03:03:20 -0300

    fav andando

--- client/src/components/Favoritos/index.jsx
+++ client/src/components/Favoritos/index.jsx
@@ -46,7 +46,7 @@ export const Favoritos = () => {
       timer: 500,
     });
 
-     //window.location.reload()
+    //window.location.reload()
     setBol(!bol);
   };
   return (
@@ -94,7 +94,7 @@ export const Favoritos = () => {
                   <span className="item-fav"> $ {e.precio}</span>
                   <div className="div-btn-eliminar-fav">
                     <button
-                     // type="button"
+                      // type="button"
                       className="btn-del-favorito"
                       value={e.id}
                       onClick={handleDeleteFav}
